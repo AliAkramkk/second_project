cd943a609a264d55c31b89a4b2e216866323785b
AliAkramkk <akramkorakkottil@gmail.com>
2024-01-19 16:17:43 +0530

    my learning page created and also route it in the front end

--- client/src/routers/User.jsx
+++ client/src/routers/User.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import RequerAuth from "../features/RequerAuth";
 import Profile from "../pages/User/UserProfile/Profile";
+import MyLearning from "../pages/User/MyLearnings/MyLearning";
 
 const Role = 2000;
 function User() {
@@ -9,6 +10,7 @@ function User() {
     <Routes>
       <Route element={<RequerAuth allows={[Role]} />}>
         <Route exact path="/profile" element={<Profile />} />
+        <Route path="/my-learning" element={<MyLearning />} />
       </Route>
     </Routes>
   );
